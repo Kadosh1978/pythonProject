d719c2b9d6e738fb575bc0b18c803ddb09bf8548
kadosh <Noone19780506@outlook.com>
2023-11-02 11:38:37 +0300

    New

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def set_color(text, color):
     return color + text + Color.reset
 
 class Cell(object):
-    empty_cell = set_color(' ', Color.yellow2)
+    empty_cell = set_color('О', Color.yellow2)
     ship_cell = set_color('■', Color.blue)
     destroyed_ship = set_color('X', Color.yellow)
     damaged_ship = set_color('□', Color.red)
